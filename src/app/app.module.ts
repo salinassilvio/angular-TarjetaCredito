b66e751612ad664f68c92aa4a4d3d52360ed10a5
Silvio Salinas <ssalinas.silvio@gmail.com>
2021-11-03 23:13:36 -0600

    first commit
    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
+import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrearTarjetaComponent,
+    ListarTarjetaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/TarjetaCredito.ts
@@ -0,0 +1,20 @@
+import { trigger } from "@angular/animations";
+
+export class TarjetaCredito{
+    id?: string;
+    titular: string;
+    numeroTarjeta: string;
+    fechaExpiracion: string;
+    cvv: number;
+    fechaCreacion: Date;
+    fechaActualizacion: Date;
+
+    constructor(titular: string, numeroTarjeta: string, fechaExpiracion: string, cvv: number){
+        this.titular = titular;
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaExpiracion = fechaExpiracion;
+        this.cvv = cvv;
+        this.fechaCreacion = new Date();
+        this.fechaActualizacion = new Date();
+    }
+}
